f9959386a6f510bf4cdc69b4255ef1781f52f883
Lorenz Ras <lenzras@gmail.com>
2021-07-12 14:43:14 +0900

    Update onEditShowHandler.js

--- app-hotelYoyaku/src/pages/onEditShowHandler.js
+++ app-hotelYoyaku/src/pages/onEditShowHandler.js
@@ -1,7 +1,16 @@
 /* eslint-disable no-unused-vars */
 const onEditShowHandler = (event) => {
+  const {
+    record: {
+      部屋: {
+        value: tableRows,
+      },
+    },
+  } = event;
+
   console.log('Edit Page');
   console.log(event);
+  console.log(tableRows);
 };
 
 export default onEditShowHandler;
