bd4eccbe322534841f19f9c96ca2a4e9c13bd205
Lorenz Ras <lenzras@gmail.com>
2021-07-12 14:24:58 +0900

    added eslint

--- app-hotelYoyaku/src/app.js
+++ app-hotelYoyaku/src/app.js
@@ -1,6 +1,8 @@
-import { onIndexShow } from '../../kintone-api/api';
-import indexHandler from './pages/indexHandler';
+import { onIndexShow, onEdit } from '../../kintone-api/api';
+import onIndexShowHandler from './pages/onIndexShowHandler';
+import onEditShowHandler from './pages/onEditShowHandler';
 
 (() => {
-  kintone.events.on(onIndexShow, indexHandler);
+  kintone.events.on(onIndexShow, onIndexShowHandler);
+  kintone.events.on(onEdit, onEditShowHandler);
 })();
