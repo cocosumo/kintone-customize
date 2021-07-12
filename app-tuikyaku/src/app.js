fdf57f5eeac0ce75c51a5743a74dcbabf5556f20
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-07-12 13:18:36 +0900

    Update app.js

--- app-tuikyaku/src/app.js
+++ app-tuikyaku/src/app.js
@@ -3,4 +3,14 @@ import onIndexShowHandler from './eventHandlers/onIndexShowHandler';
 
 (() => {
   kintone.events.on(onIndexShow, onIndexShowHandler);
+  kintone.events.on('app.record.edit.show', function(event) {
+    //対象のレコードの編集を無効にする
+    var record = event.record;
+    
+    record['文字列__1行__1'].disabled = true;
+    record['文字列__1行__0'].disabled = true;
+
+    console.log("test")
+    return event;
+});
 })();
