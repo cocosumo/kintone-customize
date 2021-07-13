d4d4a636cdf5cf08bab5ca6101fde21ab7c1d161
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-07-13 11:27:00 +0900

    recordname_update

--- app-tuikyaku/src/app.js
+++ app-tuikyaku/src/app.js
@@ -1,13 +1,10 @@
-import { onIndexShow } from '../../kintone-api/api';
-
 (() => {
-  kintone.events.on(onIndexShow, onIndexShowHandler);
   kintone.events.on('app.record.edit.show', (event) => {
     // 対象のレコードの編集を無効にする
     const { record } = event;
 
-    record['文字列__1行__1'].disabled = true;
-    record['文字列__1行__0'].disabled = true;
+    record['顧客番号'].disabled = true;
+    record['顧客名'].disabled = true;
 
     console.log('test');
     return event;
