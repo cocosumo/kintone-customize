f4922076a62f93f6877b729f60f2dab7e2f70d07
Lorenz Ras <lenzras@gmail.com>
2021-08-02 11:11:14 +0900

    added new project
    futaiBattle-8

---
+++ app-futaiBattle-8/src/eventHandlers/onIndexShowHandler.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const onIndexShowHandler = (event) => {
+  const condition = kintone.app.getQueryCondition();
+  console.log(condition);
+  console.log('Index Page');
+};
+
+export default onIndexShowHandler;
--- app-付帯バトル/js/sendNewRecordToChatwork.js
+++ app-付帯バトル/js/sendNewRecordToChatwork.js
@@ -1,80 +1,76 @@
 (function () {
-    "use strict";
+  const events = [
+    'app.record.edit.submit',
+    'app.record.create.submit.success',
+    'mobile.app.record.edit.submit',
+    'mobile.app.record.create.submit.success',
+  ];
+  kintone.events.on(events, (event) => {
+    console.log(event.type);
+    const subdomain = 'rdmuhwtt6gx7';
+    const cw_token = '7bc795ef967064f642aa70956cde3cad';
+    // const room_id = '225800073'; //test
+    // const room_id = '213232379'; //RPA
+    const room_id = '6732051'; // 本番
 
-    const events = [
-        "app.record.edit.submit",
-        "app.record.create.submit.success",
-        "mobile.app.record.edit.submit",
-        "mobile.app.record.create.submit.success"
-    ]
-    kintone.events.on(events, function (event) {
-        console.log(event.type);
-        const subdomain = "rdmuhwtt6gx7";
-        const cw_token = '7bc795ef967064f642aa70956cde3cad';
-        //const room_id = '225800073'; //test
-        //const room_id = '213232379'; //RPA
-        const room_id = '6732051'; //本番
+    // 初期設定ここまで
+    const { record } = event;
 
-        //初期設定ここまで
-        const record = event.record;
+    sendMessageToChatWork(setMessage());
 
-        sendMessageToChatWork(setMessage());
+    /* Functions */
+    function setMessage() {
+      let body = '';
 
+      body += '付帯収益バトル報告です。';
+      body += `\n\n 日付：${record['契約日'].value}`;
+      body += `\n 種類：${record['種類'].value}`;
+      body += `\n 金額：${numberWithCommas(record['契約金額'].value)}円`;
+      body += `\n 担当：${getTableDetails()}`;
+      body += `\n\n ${resolveEvent()}詳細は下記URLを参照してください。`;
+      body += `\n https://${subdomain}.cybozu.com/k/${event.appId}/show#record=${event.recordId}`;
+      return body;
+    }
 
-        /* Functions */
-        function setMessage() {
-            var body = "";
+    function resolveEvent() {
+      const eventType = event.type;
+      if (eventType.includes('create')) {
+        return '新着です。';
+      } if (eventType.includes('edit')) {
+        return '編集したレコードです。';
+      }
+    }
 
-            body += "付帯収益バトル報告です。";
-            body += "\n\n 日付：" + record["契約日"]["value"];
-            body += "\n 種類：" + record["種類"]["value"];
-            body += "\n 金額：" + numberWithCommas(record["契約金額"]["value"]) + "円";
-            body += "\n 担当：" + getTableDetails();
-            body += `\n\n ${resolveEvent()}詳細は下記URLを参照してください。`
-            body += "\n https://" + subdomain + ".cybozu.com/k/" + event.appId + "/show#record=" + event.recordId;
-            return body;
-        }
+    function getTableDetails() {
+      const table = record['担当者'].value;
+      const tableContents = table.map((item) => {
+        console.log(item);
+        return `${item.value['担当者名'].value} (${item.value['ポイント'].value} pts)`;
+      });
+      return tableContents.join(' と ');
+    }
 
-        function resolveEvent() {
-            const eventType = event.type;
-            if (eventType.includes("create")) {
-                return "新着です。";
-            } else if (eventType.includes("edit")) {
-                return "編集したレコードです。";
-            }
-        }
+    function sendMessageToChatWork(body) {
+      const self_unread = 1;
 
-        function getTableDetails() {
-            const table = record["担当者"]["value"];
-            const tableContents = table.map(item => {
-                console.log(item);
-                return item["value"]["担当者名"]["value"] + " (" + item["value"]["ポイント"]["value"] + " pts)";
-            });
-            return tableContents.join(" と ");
-        }
+      const url = `https://api.chatwork.com/v2/rooms/${room_id}/messages`;
+      const headers = {
+        'X-ChatWorkToken': cw_token,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+      const params = `body=${body}&self_unread=${self_unread}`;
 
-        function sendMessageToChatWork(body) {
-            const self_unread = 1;
-            
-            var url = 'https://api.chatwork.com/v2/rooms/' + room_id + '/messages';
-            var headers = {
-                'X-ChatWorkToken': cw_token,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            };
-            var params = 'body=' + body + "&self_unread=" + self_unread;
+      kintone.proxy(url, 'POST', headers, params).then((args) => {
+        // success
+        console.log(args[1], JSON.parse(args[0]), args[2]);
+      }, (error) => {
+        // error
+        console.log(error); // proxy APIのレスポンスボディ(文字列)を表示
+      });
+    }
 
-            kintone.proxy(url, 'POST', headers, params).then(function (args) {
-                //success
-                console.log(args[1], JSON.parse(args[0]), args[2]);
-            }, function (error) {
-                //error
-                console.log(error);  //proxy APIのレスポンスボディ(文字列)を表示
-            });
-        }
-
-        function numberWithCommas(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-
-    });
-})();
+    function numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  });
+}());
