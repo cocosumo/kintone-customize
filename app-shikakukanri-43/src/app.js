397ffd09da35201651e080ec01a816a1c0cc9537
Lorenz Ras <lenzras@gmail.com>
2021-09-02 12:33:14 +0900

    shikakukanri update

--- app-shikakukanri-43/src/app.js
+++ app-shikakukanri-43/src/app.js
@@ -2,7 +2,15 @@ import { onEditOrCreate, onFieldChange, onIndexShow } from '../../kintone-api/ap
 import choiceHandler from './handlers/choiceHandler';
 import onIndexShowHandler from './pages/onIndexShowHandler';
 
+const test = () => {
+  kintone.api('/k/api/people/user/list', 'POST', {}).then(
+    ({ result }) => console.log(result),
+  );
+};
+
 (() => {
+  test();
+
   kintone.events.on(onIndexShow, onIndexShowHandler);
   kintone.events.on(onEditOrCreate.concat(onFieldChange('qualificationChoices')), choiceHandler);
 })();
