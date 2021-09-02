c154d76b56d9fba77cd50e46b80def6ff26efbac
Lorenz Ras <lenzras@gmail.com>
2021-09-02 16:31:04 +0900

    testing settings retrieval

--- app-108-yasumikanri/src/backend/settings.js
+++ app-108-yasumikanri/src/backend/settings.js
@@ -1,7 +1,8 @@
 import { fetchSettings } from '../../../kintone-api/fetchRecords';
-import { ISOToLuxon } from '../helpers/time';
+import { ISOtoLuxon } from '../helpers/time';
 
 const getYasumiCount = async (date) => {
+  const luxonDate = ISOtoLuxon(date);
   const { 設定: { value: settings } } = (await fetchSettings()).records[0];
   console.log(settings, luxonDate);
 };
--- app-108-yasumikanri/src/pages/onIndexShowHandler.js
+++ app-108-yasumikanri/src/pages/onIndexShowHandler.js
@@ -1,11 +1,9 @@
 import { render } from 'react-dom';
 import YasumiRegistry from '../forms/YasumiRegistry';
-import getYasumiCount from '../backend/settings';
 
 const registrationViewId = 5523653;
 
 const renderRegistration = async () => {
-  console.log(await getYasumiCount());
   render(<YasumiRegistry />, document.getElementById('root'));
 };
 
