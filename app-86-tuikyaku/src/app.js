ca7c154cc89941a173fe677f700908f8bebbf5aa
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-09-21 11:11:36 +0900

    ファイル名誤記修正

--- app-86-tuikyaku/src/app.js
+++ app-86-tuikyaku/src/app.js
@@ -1,5 +1,5 @@
 import recordeditshow from './functions/recordeditshow';
-import recoedindexshow from './functions/recoedindexshow';
+import recordindexshow from './functions/recordindexshow';
 
 (() => {
   // [レコード編集画面]対象のレコードの編集を無効にする
@@ -7,6 +7,6 @@ import recoedindexshow from './functions/recoedindexshow';
   kintone.events.on('mobile.app.record.edit.show', recordeditshow);
 
   // [レコード一覧画面]プルダウンによる絞り込みを行う
-  kintone.events.on('app.record.index.show', recoedindexshow);
-  kintone.events.on('mobile.app.record.index.show', recoedindexshow);
+  kintone.events.on('app.record.index.show', recordindexshow);
+  kintone.events.on('mobile.app.record.index.show', recordindexshow);
 })();
