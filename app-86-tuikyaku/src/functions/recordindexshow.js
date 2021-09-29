32ab62fbe3387b8cf6e9c8f154e27de51a33abff
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-09-29 15:08:21 +0900

    Update recordindexshow.js

--- app-86-tuikyaku/src/functions/recordindexshow.js
+++ app-86-tuikyaku/src/functions/recordindexshow.js
@@ -78,7 +78,7 @@ const recordindexshow = (event) => {
   let app86EmployeesD; // ローカルストレージの社員リストの保存データ
   const app86ShopListKey = 'app86店舗リスト'; // ローカルストレージの店舗リストの保存名(キー)
   let app86ShopListD; // ローカルストレージの店舗リストの保存データ
-  const divTime = 10; // 経過時間の判定に使用する閾値(初期=10800秒=3時間で設定)
+  const divTime = 10800; // 経過時間の判定に使用する閾値(初期=10800秒=3時間で設定)
 
   // 担当名に表示する氏名の取り出しをする
   if ((url.indexOf('query=') === -1) && (url.indexOf('q=') === -1)) { // URLにqueryが含まれないとき(初回)
