015f44ff8289903bce672ce7e66d4b0f52c45afa
Lorenz Ras <lenzras@gmail.com>
2021-10-13 12:22:41 +0900

    finished typescript template

---
+++ app-00-template-typscript/src/components/TestComponent.tsx
@@ -0,0 +1,23 @@
+
+/*
+ただの例です。
+実際はチームでコーディング規約を統一させるのがベストですが、
+いろいろやってみて、とりあえず自由！ｗ
+
+ある程度慣れたら、規約を明確にしましょう。
+*/
+
+const TestComponent = () => {
+  return (
+    <h1 style={{
+      'width': '100%',
+      'textAlign': 'center',
+      'color': 'red',
+      'fontWeight': 'bold'
+    }}
+    >
+      環境準備成功！おめでとうございます！いいもの作りましょう！！
+    </h1>);
+};
+
+export default TestComponent;
