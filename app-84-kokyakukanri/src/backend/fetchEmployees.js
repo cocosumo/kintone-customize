7151ace88d71fb591add48a6e707dc8ab3f218bb
Lorenz Ras <lenzras@gmail.com>
2021-10-14 13:22:57 +0900

    ...

--- app-84-kokyakukanri/src/backend/fetchEmployees.js
+++ app-84-kokyakukanri/src/backend/fetchEmployees.js
@@ -7,8 +7,16 @@ const appId = 34;
 
 export const empListKey = 'app86社員リスト';
 
+/**
+ * 絞りなしですべての社員レコードを取得
+ * @returns { Promise<KintoneRecords[]> } 取得したKintoneのレコード
+ */
 export const fetchEmployees = () => fetchRecords({ appId });
 
+/**
+ * 営業職？の担当者レコードのみ取得
+ * @returns { Promise<KintoneRecords[]> } 取得したKintoneのレコード
+ */
 export const fetchAgents = () => fetchRecords(
   {
     appId,
@@ -21,6 +29,10 @@ export const fetchAgents = () => fetchRecords(
   },
 );
 
+/**
+ * KintoneRecords[]を新な配列に変える。
+ * @returns  { Promise<Array.<{name: string, shop: string}>> }
+ */
 export const fetchNormalizedAgents = async () => (await fetchAgents())
   .map(({
     文字列＿氏名,
@@ -30,9 +42,20 @@ export const fetchNormalizedAgents = async () => (await fetchAgents())
     shop: ルックアップ＿店舗名.value,
   }));
 
+/**
+ * 変形した社員情報をlocalStorageに保存。
+ */
 export const setLocalAgents = async () => setStorageObj(empListKey, await fetchNormalizedAgents());
 
+/**
+ * localStorageから社員情報をすべて取得
+ */
 export const getLocalAgents = () => getStorageObj(empListKey);
 
+/**
+ * 店舗で絞って、社員レコードを取得
+ * @param {string} selectedShop, 店舗名
+ * @returns {Array.<{name: string, shop: string}>}, 社員レコード配列
+ */
 export const getAgentsByShop = (selectedShop) => getLocalAgents()
   .filter(({ shop }) => shop === selectedShop);
