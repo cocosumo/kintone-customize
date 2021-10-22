40ee92b7c268ec8b5e11cf360abae980f92423f7
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-10-22 15:22:54 +0900

    セレクトのonchange処理の実装移管

--- app-84-kokyakukanri/src/handlers/selectShopOnChangeHandler.js
+++ app-84-kokyakukanri/src/handlers/selectShopOnChangeHandler.js
@@ -1,22 +1,14 @@
 import {selectShopID, selectEmpID} from '../view/utilsDOM';
 import {makeEmpList} from '../backend/makeList';
+import {getLocalAgents} from '../backend/fetchEmployees';
+import {flg1st, FlgOcpChk, selectName} from '../backend/setName';
 
 /**
  * 店舗名のプルダウンが変更されたときの処理
  *
- * @param {
- *   @param TrgtArray, 対象の社員名のみが保存されている配列
- *   @param Flag1st, 初回かどうか判定するフラグ(True:初回, False:初回以外)
- *   @param FlagOcp, 営業職かどうか判定するフラグ(True:営業職(主任・店長含む), false:営業職ではない)
- *   @param TrgtName, 対象者の氏名
- * }
- * @param _params
  */
-const selectShopOnChangeHandler = (_params) => {
-  const app86EmployeesD = _params.TrgtArray;
-  const flg1st = _params.Flag1st;
-  const FlgOcpChk = _params.FlagOcp;
-  let selectName = _params.TrgtName;
+const selectShopOnChangeHandler = () => {
+  const app86EmployeesD = getLocalAgents();
   const viewall = 20; // (すべて)の一覧ID：本番用・テスト用共通 = 20
 
   const affShop = document.getElementById(selectShopID).value;
@@ -32,9 +24,10 @@ const selectShopOnChangeHandler = (_params) => {
     makeEmpList(app86EmployeesD, selectEmpID, affShop); // 社員名のリスト(プルダウン)の更新
 
     if (flg1st === false || FlgOcpChk === false) {
-      selectName = 'init'; // 店舗が変更されたときは、担当名も初期表示に戻す
+      document.getElementById(selectEmpID).value = 'init'; // 店舗が変更されたときは、担当名も初期表示に戻す
+    } else {
+      document.getElementById(selectEmpID).value = selectName; // 担当名を設定
     }
-    document.getElementById(selectEmpID).value = selectName; // 担当名を設定
   }
 };
 
--- app-84-kokyakukanri/src/pages/recordindexshow.js
+++ app-84-kokyakukanri/src/pages/recordindexshow.js
@@ -1,8 +1,7 @@
 import {getHeaderSpaceElement} from '../../../kintone-api/api';
 import {makeList} from '../backend/makeList';
-import {setViewCode, selectEmpID, selectShopID, mySelectShop, mySelectEmp} from '../view/utilsDOM';
-import selectShopOnChangeHandler from '../handlers/selectShopOnChangeHandler';
-import selectEmpOnChangeHandler from '../handlers/selectEmpOnChangeHandler';
+import {setViewCode, selectEmpID, selectShopID} from '../view/utilsDOM';
+import addEvents from '../view/events';
 import {getLocalTimes} from '../backend/timeControl';
 import {getLocalShops} from '../backend/fetchShop';
 import {getAgentsByShop, getLocalAgents} from '../backend/fetchEmployees';
@@ -13,13 +12,11 @@ import {setSelectName, flg1st, affShop, selectName, FlgOcpChk, getLists, setAffi
 const recordindexshow = (event) => {
   /* **************************************** 変数宣言部 **************************************** */
   // ローカルストレージの活用をする
-  let app86DateTimeD; // 日時の保存データ
-  let app86EmployeesD; // 社員リストの保存データ
-  let app86ShopListD; // 店舗リストの保存データ
+  const app86DateTimeD = getLocalTimes(); // 日時の保存データ
+  const app86EmployeesD = getLocalAgents(); // 社員リストの保存データ
+  const app86ShopListD = getLocalShops(); // 店舗リストの保存データ
   const divTime = 20; // 経過時間の判定に使用する閾値(初期=10800秒=3時間で設定)
 
-  /* **************************************** 関数宣言部 **************************************** */
-  /* **************************************** 処理実装部 **************************************** */
   // ボタンの増殖防止
   if (document.getElementById(selectEmpID) !== null) {
     return;
@@ -37,53 +34,13 @@ const recordindexshow = (event) => {
 
   // 担当名に表示する氏名の取り出しをする
   setSelectName();
-  /*  url = decodeURI(url); // urlをデコーディングする
-  // URLにqueryが含まれないとき(初回)
-  if ((url.indexOf('query=') === -1) && (url.indexOf('q=') === -1)) {
-    flg1st = true;
-    selectName = kintone.getLoginUser().name;
-    selectNameL = selectName.substring(0, selectName.indexOf(' '));
-    selectNameF = selectName.slice(selectName.indexOf(' ') + 1);
-    selectName = selectNameL.concat(' ', selectNameF);
-    affShop = 'init';
-    console.log('ユーザ名の取得 :', selectName);
-  } else {
-    // URLにプルダウンで指定したqueryが含まれる場合の処理
-    flg1st = false;
-    let resultPos = url.indexOf('担当名');
-    if (resultPos !== -1) { // URLに'担当名'が含まれるとき
-      // ここから：絞り込み条件の氏名を取り出す 「query=担当名 like "苗字" and 担当名 like "名前"」
-      selectName = url.slice(resultPos + 10); // 10=読み飛ばす文字数
-      resultPos = selectName.indexOf('担当名');
-      selectNameL = selectName.substring(0, resultPos - 6);
-      selectName = selectName.slice(resultPos + 10);
-      selectNameF = selectName.substring(0, selectName.indexOf('"'));
-      selectName = selectNameL.concat(' ', selectNameF);
-      affShop = 'init';
-    } else if (url.indexOf('店舗名') !== -1) { // URLに'店舗名'が含まれるとき
-      resultPos = url.indexOf('店舗名');
-      affShop = url.slice(resultPos + 10); // 10=読み飛ばす文字数
-      affShop = `${affShop.substring(0, affShop.indexOf('"'))}店`;
-      selectName = 'init';
-    } else {
-      // ユーザがプルダウン以外で絞り込み表示している場合
-      // 処理は実行しない
-    }
-  } */
-
-  // LocalStrageに日時が保存されているか確認する - (1)
-  app86DateTimeD = getLocalTimes();
-  app86EmployeesD = getLocalAgents();
-  app86ShopListD = getLocalShops();
-
-  // localStorage.removeItem(shopListKey); // debag用、のちに削除すること
-
   console.log('1-1 経過時間(秒)：', (((Date.now()) / 1000) - app86DateTimeD));
 
-  // (1-2)データが保存されていない場合 or 前回のデータの取得から3時間経過している場合
   if ((app86DateTimeD === null) || (app86EmployeesD === null)
    || (app86ShopListD === null) || (((Date.now() / 1000) - app86DateTimeD) >= divTime)) {
-    getLists(); // 社員名簿アプリ及び、店舗リストアプリから一覧情報を取得する
+    // (1-2)データが保存されていない場合 or 前回のデータの取得から3時間経過している場合
+    // 社員名簿アプリ及び、店舗リストアプリから一覧情報を取得する
+    getLists();
   } else {
     // (1-1)(3-2)ローカルストレージにデータが保存されている場合 かつ、(2-2)3時間経過していない場合
     // - - - LocalStrageに保存されている店舗リスト(app86ShopListD)・社員リスト(app86EmployeesD)で処理する
@@ -100,17 +57,7 @@ const recordindexshow = (event) => {
     setview();
   }
 
-  // 店舗名のプルダウン変更時の処理 ⇒【！！！要対応！！！】以降、view/events.jsに処理を移管する
-  const paramsShopChange = {
-    TrgtArray: app86EmployeesD,
-    Flag1st: flg1st,
-    FlagOcp: FlgOcpChk,
-    TrgtName: selectName
-  };
-  mySelectShop().onchange = () => selectShopOnChangeHandler(paramsShopChange);
-
-  // 担当者のプルダウン変更時の処理
-  mySelectEmp().onchange = () => selectEmpOnChangeHandler();
+  addEvents();
 };
 
 export default recordindexshow;
