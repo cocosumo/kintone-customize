25c10e1337e4df36f42eb0ae8352dd69550f54a3
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-10-27 13:24:40 +0900

    template相当にpackage類を修正

--- app-86-tuikyaku/src/app.js
+++ app-86-tuikyaku/src/app.js
@@ -1,7 +1,6 @@
 import {onEdit, onIndexShow} from '../../kintone-api/api';
 import recordeditshow from './functions/recordeditshow';
-import recordindexshow from './functions/recordindexshow';
-// import recordindexshow from '../../app-84-kokyakukanri/src/pages/recordindexshow';
+import recordindexshow from '../../app-84-kokyakukanri/src/pages/recordindexshow';
 
 (() => {
   // [レコード編集画面]対象のレコードの編集を無効にする
--- app-86-tuikyaku/webpack.config.js
+++ app-86-tuikyaku/webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// hello
 
 module.exports = {
   mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
 
   entry: {
     customize: './src/app.js',
@@ -9,17 +21,29 @@ module.exports = {
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist', 'js'),
+    path: path.resolve(__dirname, 'dist'),
   },
 
   resolve: {
-    extensions: ['.js', '.json'],
+    extensions: ['.js', '.json', '.jsx'],
   },
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, 'dist/css'),
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader', // https://webpack.js.org/loaders/babel-loader/#root
@@ -34,16 +58,20 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/inline',
       },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
     ],
   },
 };
