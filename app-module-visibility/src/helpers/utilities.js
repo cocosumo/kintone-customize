cad1ddebaa6d1c22e5ba85c4928f8997912b8c57
Lorenz Ras <lenzras@gmail.com>
2021-11-02 14:11:08 +0900

    Update utilities.js

--- app-module-visibility/src/helpers/utilities.js
+++ app-module-visibility/src/helpers/utilities.js
@@ -1,21 +1,10 @@
-function props(obj) {
-  const p = [];
-  for (; obj != null; obj = Object.getPrototypeOf(obj)) {
-    const op = Object.getOwnPropertyNames(obj);
-    for (let i = 0; i < op.length; i++) {
-      if (p.indexOf(op[i]) === -1) p.push(op[i]);
-    }
-  }
-  return p;
-}
-
 export const getChangedFieldDetails = (event) => {
   const {
     changes: {field},
     type
   } = event;
 
-  console.log(props(field));
+
   return {
     fieldCode: type.split('change.')[1],
     choice: field.value
