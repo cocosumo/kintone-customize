5c067df0df602a45119ae121efc99be1a96ced36
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2021-11-26 13:08:51 +0900

    「全店舗」テーブルフォーマットの巻き戻し

--- app-148-donyutashakanri/src/compornents/Table/AllStoresTable.jsx
+++ app-148-donyutashakanri/src/compornents/Table/AllStoresTable.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import {groupRecordsByField} from '../../../../app-147-baikyakukanrihyou/src/helpers/utils';
 import {getMonth, getYear} from 'date-fns';
 import SiteGroupTable from './SiteGroupTable';
-import {CellHeader, Row, Table, TableHead, TableBody} from '@yumetetsu/ui';
 
 const AllStoresTable = ({reportDate, data, componentRef}) => {
   // 媒体サイト毎に、データを再編する
@@ -11,7 +10,6 @@ const AllStoresTable = ({reportDate, data, componentRef}) => {
   // const sites = Object.keys(groupBySite); // サイト名の配列を作成(sites)
   const newMonth = getMonth(reportDate) + 1;
   const newYear = getYear(reportDate);
-  const fields = ['エリア店舗名', '導入他社数', '課金額'];
 
   // console.log('groupBySite', groupBySite);
 
@@ -20,24 +18,9 @@ const AllStoresTable = ({reportDate, data, componentRef}) => {
       <article className="print-area">
         <span className="mainTitle"> {newYear}年{newMonth}月 全店舗 </span><br />
         <span className="subTitle"> 課金一覧 </span>
-        <section className="print_pages">
-          <Table>
-            <TableHead>
-              <Row>
-                <CellHeader> サイト名 </CellHeader>
-                {fields.map(fieldname => {
-                  return <CellHeader key={fieldname}>{fieldname}</CellHeader>;
-                })}
-              </Row>
-            </TableHead>
-            <TableBody>
-              {Object.entries(groupBySite).map(([key, value]) => {
-                return <SiteGroupTable key={key} site={key} records={value} fields={fields} />;
-              })}
-
-            </TableBody>
-          </Table>
-        </section>
+        {Object.entries(groupBySite).map(([key, value]) => {
+          return <SiteGroupTable key={key} site={key} records={value} />;
+        })}
       </article>
     </div>
   );
--- app-148-donyutashakanri/src/compornents/Table/SiteGroupTable.jsx
+++ app-148-donyutashakanri/src/compornents/Table/SiteGroupTable.jsx
@@ -1,34 +1,51 @@
 import PropTypes from 'prop-types';
-import {Row, Cell} from '@yumetetsu/ui';
+import {CellHeader, Row, Table, TableHead, TableBody, Cell} from '@yumetetsu/ui';
 
-const SiteGroupTable = ({site, records, fields}) => {
+const SiteGroupTable = ({site, records}) => {
 
+  const fields = ['エリア店舗名', '導入他社数', '課金額'];
   const rowSpan = records.length;
+  console.log('records', records);
 
   return (
-    records.map((record, index) => {
-      return (
-        <Row key={site + record['エリア店舗名'].value}>
+    <section className="print_pages" key="課金一覧">
+      <Table>
+        <TableHead>
+          <Row>
+            <CellHeader> サイト名 </CellHeader>
+            {fields.map(fieldname => {
+              return <CellHeader key={fieldname}>{fieldname}</CellHeader>;
+            })}
+          </Row>
+        </TableHead>
+        <TableBody>
           {
-            (index === 0) &&
-            <td rowSpan={rowSpan} selectName="putTogether">
-              {site}
-            </td>
+            records.map((record, index) =>{
+              return (
+                <Row key={site + record['エリア店舗名'].value}>
+                  {
+                    (index === 0) &&
+                    <Cell rowSpan={rowSpan}>
+                      {site}
+                    </Cell>
+                  }
+                  {fields.map(fieldname => {
+                    return <Cell key={fieldname}>{record[fieldname].value}</Cell>;
+                  })
+                  }
+                </Row>
+              );
+            })
           }
-          {fields.map(fieldname => {
-            return <Cell key={fieldname}>{record[fieldname].value}</Cell>;
-          })
-          }
-        </Row>
-      );
-    })
+        </TableBody>
+      </Table>
+    </section>
   );
 };
 
 SiteGroupTable.propTypes = {
   site: PropTypes.string,
-  records: PropTypes.array,
-  fields: PropTypes.array
+  records: PropTypes.array
 };
 
 export default SiteGroupTable;
