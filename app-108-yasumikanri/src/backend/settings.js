4b766fef10a0bea0aa6e8f795a5edac55fc05bf0
Lorenz Ras <lenzras@gmail.com>
2021-12-03 10:01:09 +0900

    made yasumi days more flexible

--- app-108-yasumikanri/src/backend/settings.js
+++ app-108-yasumikanri/src/backend/settings.js
@@ -1,5 +1,7 @@
-import { fetchSettings } from '../../../kintone-api/fetchRecords';
-import { envAppId } from '../helpers/env';
+import {fetchSettings} from '../../../kintone-api/fetchRecords';
+
+
+const prodAppId = '108';
 
 /* Plus 1 yasumi for support roles. */
 const yasumiDaysReference = {
@@ -14,23 +16,42 @@ const calcYasumiDays = (luxonDate) => {
   return yasumiDaysReference[monthDays];
 };
 
+
+/**
+ *
+ * @param {Date} luxonDate
+ * @param {} settingsTable
+ * @returns
+ *
+ */
+const findSettingInTable = (luxonDate, settingsTable) => {
+  const {year} = luxonDate;
+  const settings = settingsTable.filter(({value}) => {
+    const {設定名: {value: settingsName}} = value;
+    return settingsName === `休み数_${year}` || settingsName === '休み数';
+  });
+
+  console.log(settings[settings.length - 1]);
+
+  return settings[settings.length - 1];
+
+};
+
 const getYasumiCount = async (luxonDate) => {
-  const { year, month } = luxonDate;
+  const {month} = luxonDate;
+  console.log(luxonDate);
 
   const employeeRole = localStorage.getItem('employeeRole');
 
   const {
-    設定: { value: settingsTable },
-  } = (await fetchSettings(envAppId())).records[0];
+    設定: {value: settingsTable},
+  } = (await fetchSettings(prodAppId)).records[0];
 
-  const yasumiDaysSetting = settingsTable.find(({ value }) => {
-    const { 設定名: { value: settingsName } } = value;
-    return settingsName === `休み数_${year}`;
-  });
+  const yasumiDaysSetting = findSettingInTable(luxonDate, settingsTable);
 
-  const yasumiDays = !yasumiDaysSetting
-    ? 0
-    : JSON.parse(yasumiDaysSetting?.value.設定値.value)[month];
+  const yasumiDays = yasumiDaysSetting
+    ? JSON.parse(yasumiDaysSetting?.value.設定値.value)[month]
+    : 0;
   return (yasumiDays || calcYasumiDays(luxonDate)) + (employeeRole === 'サポート' ? 1 : 0);
 };
 
