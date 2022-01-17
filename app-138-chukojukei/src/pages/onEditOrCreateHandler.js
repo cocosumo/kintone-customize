f2891f122c0417df15414495d71d97570e4bd267
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-01-17 11:17:54 +0900

    表示切替のcomplexityprocessを削除

--- app-138-chukojukei/src/pages/onEditOrCreateHandler.js
+++ app-138-chukojukei/src/pages/onEditOrCreateHandler.js
@@ -1,12 +1,10 @@
 import {setVisibility} from '../../../app-module-visibility/src/helpers/visibility';
 import fieldsWithVisibilitySideEffect from '../helpers/visibilitySettings.json';
 import {setFieldShown} from '../../../kintone-api/api';
-import {visibleChecks} from '../helpers/complexityProcess';
 
 const onEditOrCreateHandler = (event) => {
 
   setVisibility(event, fieldsWithVisibilitySideEffect);
-  visibleChecks(event); // 複雑なフィールド表示の、初回チェック用
 
   setFieldShown('_103_08_容積率a', false); // フィールド「_103_08_容積率a」を非表示にする
 
