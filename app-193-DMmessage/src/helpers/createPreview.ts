7543b72c43678b6354f1fdddd44dfc995e2de848
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-07-04 12:43:52 +0900

    更新 モーダルウィンドウ実装

--- app-193-DMmessage/src/helpers/createPreview.ts
+++ app-193-DMmessage/src/helpers/createPreview.ts
@@ -1,3 +1,5 @@
+import './createPreview.css';
+
 /**
  * プレビューボタンが押されたら、モーダルウィンドウにプレビューを表示する
  * @param btn HTMLButtonElement: プレビューボタン
@@ -5,28 +7,59 @@
 const createPreview = (
   btn: HTMLButtonElement,
 ) => {
-  btn.onclick = async () => {
-    // モーダルウィンドウにプレビューを表示する
-    // スペースフィールド：previewの取得
-    const space = kintone.app.record.getSpaceElement('preview') as HTMLElement;
-    space.classList.add('preview');
+  // btn.onclick = async () => {
+  // モーダルウィンドウにプレビューを表示する
+  // スペースフィールド：previewの取得
+  const modal = kintone.app.record.getSpaceElement('forDebag') as HTMLElement;
+  modal.classList.add('modal');
+  const modalContent = document.createElement('div');
+  modalContent.id = 'modal';
 
-    const inputExplan = document.createElement('label');
-    inputExplan.style.border = 'true';
-    const br = document.createElement('br');
-    const elem = document.createElement('div');
-    elem.id = 'preview';
+  // モーダルのヘッダを作成する
+  const head = document.createElement('div');
+  head.className = 'modal-header';
+  head.innerHTML = `<p class="modalTitle">メール本文のプレビュー</p>
+  <span class="modalClose">×</span>`;
+
+  // モーダルの内容を設定する
+  // const br = document.createElement('br');
+  const elem = document.createElement('div');
+  elem.id = 'modal-body';
+  // console.log('メール本文', elem);
+
+  // スペースフィールドにボタンを設定する
+  modalContent.appendChild(head);
+  modalContent.appendChild(elem);
+
+  modal.appendChild(modalContent);
+
+  // プレビューボタンが押されたとき
+  const modalOpen = () => {
     const record = kintone.app.record.get();
     const mailMain = record.record.mail_main.value;
     elem.innerHTML = mailMain;
-    console.log('メール本文', elem);
-
-    inputExplan.textContent = 'メール本文のプレビュー';
-    // スペースフィールドにボタンを設定する
-    space.appendChild(inputExplan);
-    space.appendChild(br);
-    space.appendChild(elem);
+    modal.style.display = 'block';
   };
+  btn.addEventListener('click', modalOpen);
+
+  const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+  // バツ印がクリックされた時
+  const modalClose = () => {
+    modal.style.display = 'none';
+  };
+  buttonClose.addEventListener('click', modalClose);
+
+
+  // モーダルコンテンツ以外がクリックされた時
+  const outsideClose = (e: any) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+  addEventListener('click', outsideClose);
+  // };
+
 };
 
 export default createPreview;
