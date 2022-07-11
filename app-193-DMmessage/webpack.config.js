dfc6c5c152f66f3f19808a4f2471bcd7dfcb2378
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-07-11 13:53:08 +0900

    tailwind削除、react追加

--- app-193-DMmessage/webpack.config.js
+++ app-193-DMmessage/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
               publicPath: path.resolve(__dirname, 'dist'),
             },
           },
-          'css-loader',
+            "css-loader", "postcss-loader",
         ],
       },
     ],
