1995f540d1bded9ec8409c7c3a3ee40711a5ac2d
Lorenz Ras <lenzras@gmail.com>
2024-06-08 10:37:22 +0900

    remove deprecated app

--- app-115-faceRecognition/src/index.tsx
+++
@@ -1,9 +0,0 @@
-
-import onIndexShowHandler from './handlers/kintone/onIndexShowHandler';
-
-const onIndexShow : string[] = [
-  'app.record.index.show',
-  'mobile.app.record.index.show',
-];
-
-kintone.events.on(onIndexShow, onIndexShowHandler);
